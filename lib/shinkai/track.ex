88ad28c3e4e7761e9f5b958fe300b29d89029394
Billal GHILAS <bilal@evercam.io>
2025-12-21 08:53:11 +0100

    hls: ignore unsupported tracks

--- lib/shinkai/track.ex
+++ lib/shinkai/track.ex
@@ -23,4 +23,16 @@ defmodule Shinkai.Track do
   def new(opts) do
     struct(__MODULE__, opts)
   end
+
+  @doc false
+  @spec to_hls_track(t()) :: HLX.Track.t()
+  def to_hls_track(track) do
+    HLX.Track.new(
+      id: track.id,
+      type: track.type,
+      codec: track.codec,
+      priv_data: track.priv_data,
+      timescale: track.timescale
+    )
+  end
 end
