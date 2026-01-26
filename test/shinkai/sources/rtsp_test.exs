61f6084afc763964a52a2246c4605188dcf266c7
Billal Ghilas <84322223+gBillal@users.noreply.github.com>
2026-01-26 11:25:50 +0100

    Build binaries using burrito (#17)
    * Build binaries using burrito
    
    * Add release github action
    
    * Add more tests
    
    * make credo happy

--- test/shinkai/sources/rtsp_test.exs
+++ test/shinkai/sources/rtsp_test.exs
@@ -1,75 +1,62 @@
 defmodule Shinkai.Sources.RTSPTest do
   use ExUnit.Case, async: true
 
+  import Shinkai.Test.MediaAssertion
+
   alias RTSP.FileServer
   alias Shinkai.Sources.Source
 
   setup do
-    {:ok, server} =
-      FileServer.start_link(
-        port: 0,
-        files: [%{path: "/test", location: "test/fixtures/big_buck_avc_aac.mp4"}],
-        rate_control: false
-      )
+    files =
+      fixtures()
+      |> Enum.with_index(1)
+      |> Enum.map(fn {fixture, index} -> %{path: "/test#{index}", location: fixture} end)
+
+    {:ok, server} = FileServer.start_link(port: 0, files: files, rate_control: false)
 
     %{rtsp_server: server}
   end
 
-  test "tracks received from rtsp source", %{rtsp_server: server} do
-    {:ok, port} = FileServer.port_number(server)
+  for {fixture, index} <- Enum.with_index(fixtures(), 1) do
+    test "tracks received from rtsp source: #{fixture}", %{rtsp_server: server} do
+      {:ok, port} = FileServer.port_number(server)
 
-    source = %Source{id: "test", type: :rtsp, uri: "rtsp://127.0.0.1:#{port}/test"}
-    Phoenix.PubSub.subscribe(Shinkai.PubSub, Shinkai.Utils.tracks_topic(source.id))
+      source = %Source{
+        id: "test",
+        type: :rtsp,
+        uri: "rtsp://127.0.0.1:#{port}/test#{unquote(index)}"
+      }
 
-    _pid = start_supervised!({Shinkai.Sources.RTSP, source})
+      Phoenix.PubSub.subscribe(Shinkai.PubSub, Shinkai.Utils.tracks_topic(source.id))
 
-    assert_receive {:tracks, tracks}, 2_000
-    assert length(tracks) == 2
+      _pid = start_supervised!({Shinkai.Sources.RTSP, source})
 
-    assert [
-             %Shinkai.Track{
-               id: 1,
-               type: :video,
-               codec: :h264,
-               timescale: 90_000,
-               priv_data:
-                 {<<103, 66, 192, 12, 217, 3, 196, 254, 95, 252, 2, 32, 2, 28, 64, 0, 0, 3, 0, 64,
-                    0, 0, 15, 3, 197, 10, 146>>, [<<104, 203, 131, 203, 32>>]}
-             },
-             %Shinkai.Track{
-               id: 2,
-               type: :audio,
-               codec: :aac,
-               timescale: 48_000,
-               priv_data: %MediaCodecs.MPEG4.AudioSpecificConfig{
-                 object_type: 2,
-                 sampling_frequency: 48_000,
-                 channels: 0,
-                 aot_specific_config:
-                   <<0, 153, 8, 128, 4, 0, 24, 129, 169, 140, 46, 204, 102, 167, 5, 198, 166, 133,
-                     198, 38, 6, 10, 220, 160, 0::size(3)>>
-               }
-             }
-           ] == tracks
-  end
+      assert_receive {:tracks, tracks}, 2_000
+      assert length(tracks) == 2
+      assert_tracks(unquote(fixture), tracks)
+    end
 
-  test "packets are received from rtsp source", %{rtsp_server: server} do
-    {:ok, port} = FileServer.port_number(server)
+    test "packets are received from rtsp source: #{fixture}", %{rtsp_server: server} do
+      {:ok, port} = FileServer.port_number(server)
 
-    source = %Source{id: UUID.uuid4(), type: :rtsp, uri: "rtsp://127.0.0.1:#{port}/test"}
-    Phoenix.PubSub.subscribe(Shinkai.PubSub, Shinkai.Utils.packets_topic(source.id))
-    Phoenix.PubSub.subscribe(Shinkai.PubSub, Shinkai.Utils.state_topic(source.id))
+      source = %Source{
+        id: UUID.uuid4(),
+        type: :rtsp,
+        uri: "rtsp://127.0.0.1:#{port}/test#{unquote(index)}"
+      }
 
-    _pid = start_supervised!({Shinkai.Sources.RTSP, source})
+      Phoenix.PubSub.subscribe(Shinkai.PubSub, Shinkai.Utils.packets_topic(source.id))
+      Phoenix.PubSub.subscribe(Shinkai.PubSub, Shinkai.Utils.state_topic(source.id))
 
-    packets = collect_packets()
-    assert length(packets) == 770
-    assert Enum.filter(packets, &(&1.track_id == 1)) |> length() == 300
-    assert Enum.filter(packets, &(&1.track_id == 2)) |> length() == 470
+      _pid = start_supervised!({Shinkai.Sources.RTSP, source})
 
-    FileServer.stop(server)
+      packets = collect_packets()
+      assert_received_packets(unquote(fixture), packets)
 
-    assert_receive :disconnected, 1_000
+      FileServer.stop(server)
+
+      assert_receive :disconnected, 1_000
+    end
   end
 
   defp collect_packets(acc \\ []) do
